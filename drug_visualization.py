163df3ef3d93d2456e29902ddb29572da3bd462d
ASHIKAMOHAN <ashikaadhi99@gmail.com>
2023-11-03 01:11:57 +0000

    added title,labels and legend

--- drug_visualization.py
+++ drug_visualization.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-rows = ['age','alcohol-use','alcohol-frequency','marijuana-use','marijuana-frequency','cocaine-use','cocaine-frequency','crack-use','crack-frequency','oxycontin-use','oxycontin-frequency','tranquilizer-use','tranquilizer-frequency','stimulant-use','stimulant-frequency','meth-use','meth-frequency','sedative-use','sedative-frequency']
-drugs=pd.read_csv("C:\\Users\\ashik\\Desktop\\ADS\\ASSIGNMENTS\\drug\\drug-use-by-age.csv",usecols=rows)
+#specifiying the column names and reading the csv file
+columns = ['age','alcohol-frequency','marijuana-frequency','cocaine-frequency','crack-frequency','oxycontin-frequency','tranquilizer-frequency','stimulant-frequency','meth-frequency','sedative-frequency']
+drugs=pd.read_csv("C:\\Users\\ashik\\Desktop\\ADS\\ASSIGNMENTS\\drug\\drug-use-by-age.csv",usecols=columns)
+#setting 'age' as the index
+drugs.set_index('age',inplace=True)
 print(drugs)
+#Plotting the data
 drugs.plot()
-
+#labelling the x and y axis
+plt.xlabel('age')
+plt.ylabel('Frequency')
+#adding title to the graph
+plt.title('Drug use by age',size=18)
+#Displaying graph legend
+plt.legend(drugs.columns)
 plt.show()
