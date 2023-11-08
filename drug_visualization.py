6ea58a7da8d76b25321ae9a7817c7fecd42e6083
ASHIKAMOHAN <ashikaadhi99@gmail.com>
2023-11-08 16:59:26 +0000

    updated functions for pie plot

--- drug_visualization.py
+++ drug_visualization.py
@@ -22,9 +22,19 @@ def PlotLineGraph(data,x_col,y_col,title,legend):
 #calling the PlotLineGraph with the data and column name
 PlotLineGraph(drugs,'age','alcohol-frequency','Drug use by age','drugs.drug_frequency')
 
+
+def plotpie(drug_use):
+    drugs=pd.read_csv("C:\\Users\\ashik\\Desktop\\ADS\\ASSIGNMENTS\\drug\\drug-use-by-age.csv",usecols=drug_use)
+    #setting 'age' as the index
+    drugs.set_index('age',inplace=True)
+    total_usage=drugs.sum()
+    top_five=total_usage.nlargest(5)
+    top_five['others']=total_usage.sum()-top_five.sum()
+    plt.figure()
+    plt.pie(top_five,labels=top_five.index,autopct='%1.1f%%')
+    plt.title('Drug use by age')
+    plt.show()
+
 drug_use = ['age','alcohol-use','marijuana-use','cocaine-use','crack-use','oxycontin-use','tranquilizer-use','stimulant-use','meth-use','sedative-use']
-drugs=pd.read_csv("C:\\Users\\ashik\\Desktop\\ADS\\ASSIGNMENTS\\drug\\drug-use-by-age.csv",usecols=drug_use)
-#setting 'age' as the index
-drugs.set_index('age',inplace=True)
-total_usage=drugs.sum()
+plotpie(drug_use)   
 
